e563ed737419d42ae6325d2d6cc84be98ef2d72f
nickmwangemi <nickmwangemi@gmail.com>
2021-01-02 22:03:26 +0300

    Add logic to manage app data with useReducer

---
+++ reducers/photos.js
@@ -0,0 +1,14 @@
+const types = {
+	LOADING: 'LOADING',
+	SUCCESS: 'SUCCESS',
+	FAILURE: 'FAILURE',
+}
+
+export const actionCreators = {
+	loading: () => ({ type: types.LOADING }),
+	failure: () => ({ type: types.FAILURE }),
+	success: (photos, page) => ({
+		type: types.SUCCESS,
+		payload: { photos, page },
+	}),
+}
