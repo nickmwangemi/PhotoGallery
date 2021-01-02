c2f80b62d0e64a787ce3613c9b2ef6587af38ba7
nickmwangemi <nickmwangemi@gmail.com>
2021-01-02 22:25:25 +0300

    Refactor function

--- components/PhotoGrid.js
+++ components/PhotoGrid.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Dimensions, FlatList, Image } from 'react-native'
 import { formatPhotoUri } from '../api/picsum'
 
-export default PhotoGrid = ({ photos, numColumns, onEndReached }) => {
+export default function PhotoGrid({ photos, numColumns, onEndReached }) {
 	const { width } = Dimensions.get('window')
 	const size = width / numColumns
 	return (
